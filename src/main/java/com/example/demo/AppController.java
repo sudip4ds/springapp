6f9ba692bdca496bdefa527583d525e5bf56affc
Sudip Das <dass06621@gmail.com>
2022-11-25 10:42:22 +0530

    pom

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -1,7 +1,8 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class AppController {
 	@GetMapping("home")
 	public String getHome() {
